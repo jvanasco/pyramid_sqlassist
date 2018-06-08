30a0c49365c02dae5c14c18b8ad4c3a0a9bc370b
jonathan vanasco <jonathan@2xlp.com>
2018-06-08 17:28:32 -0400

    0.9.4 updates

--- pyramid_sqlassist/objects.py
+++ pyramid_sqlassist/objects.py
@@ -3,8 +3,11 @@ log = logging.getLogger(__name__)
 
 
 import sqlalchemy
-import sqlalchemy.orm as sqlalchemy_orm
+import sqlalchemy.sql
+from sqlalchemy.orm import class_mapper as sa_class_mapper
 
+# this is used a bit
+func_lower = sqlalchemy.sql.func.lower
 
 # ==============================================================================
 
@@ -17,21 +20,23 @@ class UtilityObject(CoreObject):
 
     def get__by__id(self, dbSession, id, id_column='id'):
         """gets an item by an id column named 'id'.  id column can be overriden"""
-        if not hasattr(self.__class__, id_column) and hasattr(self, '__table_pkey__'):
+        _cls = self.__class__
+        if not hasattr(_cls, id_column) and hasattr(self, '__table_pkey__'):
             id_column = self.__table_pkey__
-        id_col = getattr(self.__class__, id_column)
+        id_col = getattr(_cls, id_column)
         if isinstance(id, (list, tuple)):
-            return dbSession.query(self.__class__).filter(id_col.in_(id)).all()
+            return dbSession.query(_cls).filter(id_col.in_(id)).all()
         else:
             id_dict = {id_column: id}
-            return dbSession.query(self.__class__).filter_by(**id_dict).first()
+            return dbSession.query(_cls).filter_by(**id_dict).first()
 
     def get__by__column__lower(self, dbSession, column, search, allow_many=False):
         """gets items from the database based on a lowercase version of the column.
         useful for situations where you have a function index on a table
         (such as indexing on the lower version of an email addresses)"""
-        items = dbSession.query(self.__class__)\
-            .filter(sqlalchemy.sql.func.lower(getattr(self.__class__, column)) == search.lower(),
+        _cls = self.__class__
+        items = dbSession.query(_cls)\
+            .filter(func_lower(getattr(_cls, column)) == search.lower(),
                     )\
             .all()
         if items:
@@ -46,46 +51,49 @@ class UtilityObject(CoreObject):
 
     def get__by__column__similar(self, dbSession, column, seed, prefix_only=True):
         """seaches for a name column entry with the submitted seed prefix"""
-        query = dbSession.query(self.__class__)
+        _cls = self.__class__
+        query = dbSession.query(_cls)
         if prefix_only:
-            query = query.filter(sqlalchemy.sql.func.lower(getattr(self.__class__, column)).startswith(seed.lower()),
+            query = query.filter(func_lower(getattr(_cls, column)).startswith(seed.lower()),
                                  )
         else:
-            query = query.filter(sqlalchemy.sql.func.lower(getattr(self.__class__, column)).contains(seed.lower()),
+            query = query.filter(func_lower(getattr(_cls, column)).contains(seed.lower()),
                                  )
         results = query\
-            .order_by(getattr(self.__class__, column).asc())\
+            .order_by(getattr(_cls, column).asc())\
             .all()
         return results
 
     def get__by__column__exact_then_ilike(self, dbSession, column, seed):
         """ seaches for an exact, then case-insensitive version of the name column"""
-        item = dbSession.query(self.__class__).filter(getattr(self.__class__, column) == seed).first()
+        _cls = self.__class__
+        item = dbSession.query(_cls).filter(getattr(_cls, column) == seed).first()
         if not item:
-            item = dbSession.query(self.__class__).filter(getattr(self.__class__, column).ilike(seed)).first()
+            item = dbSession.query(_cls).filter(getattr(_cls, column).ilike(seed)).first()
         return item
 
     def get__range(self, dbSession, start=0, limit=None, sort_direction='asc', order_col=None, order_case_sensitive=True, filters=[], debug_query=False):
         """gets a range of items"""
+        _cls = self.__class__
         if not order_col:
             order_col = 'id'
-        query = dbSession.query(self.__class__)
+        query = dbSession.query(_cls)
         for filter in filters:
             query = query.filter(filter)
         for col in order_col.split(','):
             # declared columns do not have self.__class__.c
             # reflected columns did in earlier sqlalchemy
-            col = getattr(self.__class__, col)
+            col = getattr(_cls, col)
             if sort_direction == 'asc':
                 if order_case_sensitive:
                     query = query.order_by(col.asc())
                 else:
-                    query = query.order_by(sqlalchemy.sql.func.lower(col).asc())
+                    query = query.order_by(func_lower(col).asc())
             elif sort_direction == 'desc':
                 if order_case_sensitive:
                     query = query.order_by(col.desc())
                 else:
-                    query = query.order_by(sqlalchemy.sql.func.lower(col).desc())
+                    query = query.order_by(func_lower(col).desc())
             else:
                 raise ValueError('invalid sort direction')
         query = query.offset(start).limit(limit)
@@ -97,7 +105,23 @@ class UtilityObject(CoreObject):
         return results
 
     def columns_as_dict(self):
-        return dict((col.name, getattr(self, col.name)) for col in sqlalchemy_orm.class_mapper(self.__class__).mapped_table.c)
+        """
+        Beware: this function will trigger a load of attributes if they have not been loaded yet.
+        """
+        return dict((col.name, getattr(self, col.name))
+                    for col
+                    in sa_class_mapper(self.__class__).mapped_table.c
+                    )
+
+    def loaded_columns_as_dict(self):
+        """
+        This function will only return the loaded columns as a dict.
+        """
+        _dict = self.__dict__
+        return {col.name: _dict[col.name]
+                for col in sa_class_mapper(self.__class__).mapped_table.c
+                if col.name in _dict
+                }
 
 
 class ReflectedTable(UtilityObject):
