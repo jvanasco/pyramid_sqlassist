63d92d441c0aedb7e49001d98f617da3856afd55
jonathan vanasco <jonathan@2xlp.com>
2025-09-01 16:10:57 -0400

    packaging fixes

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 """pyramid_sqlassist installation script.
 """
+
 import os
 import re
 
@@ -12,9 +13,9 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(HERE, "src", "pyramid_sqlassist", "__init__.py")) as v_file:
     VERSION = re.compile(r'.*__VERSION__ = "(.*?)"', re.S).match(v_file.read()).group(1)
 
-long_description = (
-    description
-) = "Efficiently manage multiple SqlAlchemy connections for Pyramid"
+long_description = description = (
+    "Efficiently manage multiple SqlAlchemy connections for Pyramid"
+)
 with open(os.path.join(HERE, "README.md")) as fp:
     long_description = fp.read()
 
@@ -51,15 +52,17 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.13",
     ],
     keywords="web Pyramid SQLAlchemy",
-    packages=find_packages(
-        where="src",
-    ),
+    packages=find_packages(where="src"),
     package_dir={"": "src"},
-    package_data={"pyramid_sqlassist": ["py.typed"]},
-    include_package_data=True,
+    package_data={
+        "": [
+            "py.typed",
+            "debugtoolbar/panels/templates/*",
+        ],
+    },
     author="Jonathan Vanasco",
     author_email="jonathan@findmeon.com",
     url="https://github.com/jvanasco/pyramid_sqlassist",
